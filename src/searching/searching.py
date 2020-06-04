af315cf1282032ed3629dd508af886476bcf91b3
Heather Ridgill <Heather.Ridgill@gmail.com>
2020-06-03 18:40:19 -0700

    Completed binary_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,8 +7,47 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    # search space is A[left..right]
+    (left, right) = (0, len(arr) - 1)
 
-    # Your code here
+    # till search space consists of at-least one element
+    while left <= right:
+
+        # we find the mid value in the search space and
+        # compares it with key value
+
+        mid = (left + right) // 2
+
+        # overflow can happen. Use:
+        # mid = left + (right - left) / 2
+        # mid = right - (right - left) // 2
+
+        # key value is found
+        if target == arr[mid]:
+            return mid
+
+        # discard all elements in the right search space
+        # including the mid element
+        elif target < arr[mid]:
+            right = mid - 1
+
+        # discard all elements in the left search space
+        # including the mid element
+        else:
+            left = mid + 1
+
+    # x doesn't exist in the list
+    return -1
 
 
-    return -1  # not found
+# if __name__ == '__main__':
+
+#     A = [2, 5, 6, 8, 9, 10]
+#     key = 5
+
+#     index = binarySearch(A, key)
+
+#     if index != -1:
+#         print("Element found at index", index)
+#     else:
+#         print("Element found not in the list")
